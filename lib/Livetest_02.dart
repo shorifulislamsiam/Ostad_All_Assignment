2ce6570f9f2c085ff1b4e0cba558c4b27f0152e1
siam <shorifulislamsiam18@gmail.com>
2025-02-05 23:23:55 +0600

    livetest2 update

--- lib/Livetest_02.dart
+++ lib/Livetest_02.dart
@@ -32,11 +32,89 @@ class Livetest02 extends StatefulWidget {
 }
 
 class _Livetest02State extends State<Livetest02> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _ageController = TextEditingController();
+  TextEditingController _salaryController = TextEditingController();
+  var _formkey = GlobalKey<FormState>();
+  bool _isSubmitted = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Add Employe",style: TextStyle(
+                  fontSize: 22, fontWeight: FontWeight.bold,
+                ),
+                  //textAlign: TextAlign.left,
+                ),
+              ),
+              SizedBox(height: 10,),
+              Form(
+                key: _formkey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          label: Text("Name"),
+                          hintText: "Enter the Name",
+
+                        ),
+                        validator: (value){
+                          if(value != null || value!.isNotEmpty){
+                            return null;
+                          }return "Name cann't be null";
+                        },
+                      ),
+                      TextFormField(
+                        controller: _ageController,
+                        decoration: InputDecoration(
+                          label: Text("Age"),
+                          hintText: "Enter the Age",
+
+                        ),
+                        validator: (value){
+                          if(value != null || value!.isNotEmpty){
+                            return null;
+                          }return "Age can't be null";
+                        },
+                      ),
+                      TextFormField(
+                        controller: _salaryController,
+                        decoration: InputDecoration(
+                          label: Text("Salary"),
+                          hintText: "Enter the Salary",
+
+                        ),
+                        validator: (value){
+                          if(value != null || value!.isNotEmpty){
+                            return null;
+                          }return "salary cann't be null";
+                        },
+                      ),
+                    ],
+                  ),
+              ),
+              SizedBox(height: 15,),
+              SizedBox(
+                child: ElevatedButton(
+                    onPressed: (){
+                      if (_formkey.currentState!.validate()){
+                        _isSubmitted =true;
+                        print("Employee added successfully");
+                      }
+                    },
+                    child: Text("Add Employee"),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
